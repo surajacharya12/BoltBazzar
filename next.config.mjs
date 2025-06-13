be077af05d8d3408097c57fe2214f9a3f5559809
surajacharya12 <surajacharya.080@kathford.edu.np>
2025-06-13 22:38:02 +0545

    home

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: true,
-  experimental: {
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {},
   // DON'T add css-loader or style-loader manually
   webpack: (config) => {
     // you can modify config, but don't add CSS loaders here unless necessary
     return config;
-  },};
+  },
+};
 
 export default nextConfig;
